9b14c8c4c81e93485f0ab77fb36b2db17e0d7299
overthinker-pro <noelvabraham@gmail.com>
2025-02-19 08:29:26 +0530

    first commit

---
+++ work.js
@@ -0,0 +1,29 @@
+// let root = document.getElementById("root");
+
+// fetch("https://jsonplaceholder.typicode.com/photos")
+//   .then((result) => result.json())
+//   .then((posts) => {
+//     for (let i = 0; i < 60; i++) { 
+//       let card = document.createElement("div");
+//       let title = document.createElement("h2");
+//       let img = document.createElement("img");
+
+//       title.innerText = posts[i].id + " - " + posts[i].title;
+//       img.src = posts[i].thumbnailUrl;
+//       img.alt = posts[i].title;
+
+//       card.className = "card";
+//       card.append(title);
+//       card.append(img);
+
+//       root.append(card);
+//     }
+//   })
+//   .catch((error) => console.log("Error:", error));
+
+
+function a(){
+    console.log('Something is here');
+}
+
+a()
