774e369cfeb52f6a944373d3f70e11f41d3c0bb6
Noel V Abraham <noelvabraham@gmail.com>
2025-03-31 17:54:28 +0000

    code addition

--- work.js
+++ work.js
@@ -1,25 +1,25 @@
-// let root = document.getElementById("root");
+let root = document.getElementById("root");
 
-// fetch("https://jsonplaceholder.typicode.com/photos")
-//   .then((result) => result.json())
-//   .then((posts) => {
-//     for (let i = 0; i < 60; i++) { 
-//       let card = document.createElement("div");
-//       let title = document.createElement("h2");
-//       let img = document.createElement("img");
+fetch("https://jsonplaceholder.typicode.com/photos")
+  .then((result) => result.json())
+  .then((posts) => {
+    for (let i = 0; i < 60; i++) { 
+      let card = document.createElement("div");
+      let title = document.createElement("h2");
+      let img = document.createElement("img");
 
-//       title.innerText = posts[i].id + " - " + posts[i].title;
-//       img.src = posts[i].thumbnailUrl;
-//       img.alt = posts[i].title;
+      title.innerText = posts[i].id + " - " + posts[i].title;
+      img.src = posts[i].thumbnailUrl;
+      img.alt = posts[i].title;
 
-//       card.className = "card";
-//       card.append(title);
-//       card.append(img);
+      card.className = "card";
+      card.append(title);
+      card.append(img);
 
-//       root.append(card);
-//     }
-//   })
-//   .catch((error) => console.log("Error:", error));
+      root.append(card);
+    }
+  })
+  .catch((error) => console.log("Error:", error));
 
 
 // Functions can be assigned to variables
